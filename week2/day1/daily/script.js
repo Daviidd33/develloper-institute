2314de47ea88b6f91f800423ba00813428065bea
David <dav7@hotmail.fr>
2023-01-23 09:24:34 +0200

    exercise wk d1

---
+++ week2/day1/daily/script.js
@@ -0,0 +1,17 @@
+// // Exercice 1
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// fruits.shift();// supprime le 1er element
+// fruits.sort(); //trie par ordre alphabet
+// fruits.push('kiwi') //ajoute un element à la fin de l'array
+// fruits.splice(0, 1) //supprime un element (endroit, combien)
+// fruits.reverse() par odre inverse alphabetique
+// console.log(fruits);
+
+
+// Exeercise 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1]);
---
+++ week2/day1/exercise/script.js
@@ -0,0 +1,173 @@
+// Exercice 1
+
+let favoriteFood = "falafel"
+let favoriteMeal = "dinner"
+console.log(`i eat ${favoriteFood} at every ${favoriteMeal}`);
+
+// Exercice 2
+
+// let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+// let myWatchedSeriesLength = myWatchedSeries.length;
+// console.log(myWatchedSeriesLength);
+
+// let myWatchedSeriesSentence = "got, narcos, fauda"
+// console.log(`i watched ${myWatchedSeriesLength}: ${myWatchedSeriesSentence}`);
+
+// myWatchedSeries[2] = "friends"
+// console.log(myWatchedSeries);
+// myWatchedSeries.push("plus belle la vie")
+// console.log(myWatchedSeries);
+// myWatchedSeries.unshift("el chapo")
+// console.log(myWatchedSeries);
+// myWatchedSeries.splice(1, 1)
+// console.log(myWatchedSeries);
+// console.log(myWatchedSeries[1][2]);
+
+// Exercice 3
+
+// let celsiuce = 20;
+// console.log(celsiuce);
+// let fahrenheit = celsiuce * 9 / 5 + 32
+// console.log(`${celsiuce}°C is ${fahrenheit}F`);
+
+// // Exercice 4
+// let c;
+// let a = 34;
+// let b = 21;
+
+// console.log(a + b) //first expression
+// // Prediction: 34+21 = 55
+// // Actual:
+
+// a = 2;
+
+// console.log(a + b) //second expression
+// // Prediction:2 +21 = 23
+// // Actual:
+
+// // Analyse the code below, what will be the outcome?
+// console.log(3 + 4 + '5');
+
+
+// Exercice 5
+// typeof (15)
+// // Prediction: number
+// // Actual:
+
+// typeof (5.5)
+// // Prediction: number
+// // Actual:
+
+// typeof (NaN)
+// // Prediction: ujnddfined
+// // Actual:
+
+// typeof ("hello")
+// // Prediction: string
+// // Actual:
+
+// typeof (true)
+// // Prediction: bollean
+// // Actual:
+
+// typeof (1 != 2)
+// // Prediction:bollean
+// // Actual:
+
+// "hamburger" + "s"
+// // Prediction: hambugers
+// // Actual:
+
+// "hamburgers" - "s"
+// // Prediction: nan
+// // Actual:
+
+// "1" + "3"
+// // Prediction: 13
+// // Actual:
+
+// "1" - "3"
+// // Prediction: -2
+// // Actual:
+
+// "johnny" + 5
+// // Prediction:johnny5
+// // Actual:
+
+// "johnny" - 5
+// // Prediction:nan
+// // Actual:
+
+// 99 * "hello"
+// // Prediction:nan
+// // Actual:
+
+// 1 != 1
+// // Prediction: false
+// // Actual:
+
+// 1 == "1"
+// // Prediction: false
+// // Actual:
+
+// 1 === "1"
+// // Prediction: false
+// // Actual:
+
+
+// Exercice 6
+5 + "34"
+// Prediction:534
+// Actual:
+
+5 - "4"
+// Prediction:1
+// Actual:
+
+10 % 5
+// Prediction:
+// Actual:
+
+5 % 10
+// Prediction:
+// Actual:
+
+"Java" + "Script"
+// Prediction:javascript
+// Actual:
+
+" " + " "
+// Prediction:""
+// Actual:
+
+" " + 0
+// Prediction:"0"
+// Actual:
+
+true + true
+// Prediction: true
+// Actual:
+
+true + false
+// Prediction:
+// Actual:
+
+false + true
+// Prediction:
+// Actual:
+
+false - true
+// Prediction: 
+// Actual:
+
+!true
+// Prediction:
+// Actual:
+
+3 - 4
+// Prediction:
+// Actual:
+
+"Bob" - "bill"
+// Prediction:
+// Actual:
